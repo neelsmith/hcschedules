4c4686f1c9e44b80c69804f28901a01ea280e96b
Neel Smith <classics-chair@hlycross.edu>
2018-01-14 16:06:57 -0500

    Document text format

--- shared/src/main/scala/Course.scala
+++ shared/src/main/scala/Course.scala
@@ -15,19 +15,34 @@ case class Course(courseNum : String, title: String, instructor: Instructor,
   courseSlot: Option[CourseDays], hour: String)
 
 
-  object Course {
+/** Factory object for creating [[Course]]s from a delimited text string.
+* Each row should have the following columns in this sequence:
+*
+*  1. course number
+*  2. course title
+*  3. name of instructor
+*  4. area requirements satisfied
+*  5. cross listings
+*  6. days course meets
+*  7. meeting time
+*  8. course capacity
+*
+* @param row A single row of data representing one course.
+* @param sep String delimiting columns in a row of data.
+*/
+object Course {
 
-    def apply(row: String, sep: String = "\t") : Course = {
-      val cols = row.split(sep)
-      val courseNum = cols(0)
-      val title = cols(1)
-      val prof = Instructor(cols(2))
-      val areas = cols(3)
-      // for now, ignore column 4
+  def apply(row: String, sep: String = "\t") : Course = {
+    val cols = row.split(sep)
+    val courseNum = cols(0)
+    val title = cols(1)
+    val prof = Instructor(cols(2))
+    val areas = cols(3)
+    // for now, ignore column 4
 
-      val slot = courseDaysFromString(cols(5))
-      val hr = cols(6)
+    val slot = courseDaysFromString(cols(5))
+    val hr = cols(6)
 
-      Course(courseNum, title, prof, areas,slot, hr)
-    }
+    Course(courseNum, title, prof, areas,slot, hr)
   }
+}
--- shared/src/main/tut/index.md
+++ shared/src/main/tut/index.md
@@ -4,4 +4,14 @@ layout: page
 ---
 
 Work with delimited text data to manage class schedules on the Holy Cross academic calendar.
-  
+
+Each row should have the following columns in this sequence:
+
+1. course number
+2. course title
+3. name of instructor
+4. area requirements satisfied
+5. cross listings
+6. days course meets
+7. meeting time
+8. course capacity
