d02c031a5ae14374d7069b86ec4bdf3d894317cb
Neel Smith <dnsmith.neel@gmail.com>
2018-01-14 18:34:21 -0500

    Update API docs

--- shared/src/main/scala/CourseDays.scala
+++ shared/src/main/scala/CourseDays.scala
@@ -4,8 +4,14 @@ package edu.holycross.shot.courses
 /**  Allowed patterns of course days in Holy Cross academic calendar. */
 sealed trait CourseDays
 
-
+/** Monday-Wednesday course pattern. */
 case object MW extends CourseDays
+
+/** Monday-Wednesday-Friday course pattern. */
 case object MWF extends CourseDays
+
+/** Wednesday-Friday course pattern. */
 case object WF extends CourseDays
+
+/** Tuesday-Thursday course pattern. */
 case object TR extends CourseDays
