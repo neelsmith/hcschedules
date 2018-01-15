5c02c9880b9d738fc846212a3ca62faf0341201d
Neel Smith <dnsmith.neel@gmail.com>
2018-01-15 04:48:31 -0500

    Test file reader

--- jvm/src/test/scala/ScheduleSourceSpec.scala
+++ jvm/src/test/scala/ScheduleSourceSpec.scala
@@ -5,12 +5,22 @@ import org.scalatest.FlatSpec
 
 class ScheduleSourceSpec extends FlatSpec {
 
-  "A ScheduleSourceSpec"  should "construct a map of schedules from a local file" in  pending /*{
-    val fName = "jvm/src/test/resources/courses-S18.tsv"
-    val courses = CourseVectorSource(fName)
-    val expectedCourses = 37
-    assert(courses.size == expectedCourses)
-  }*/
+  val fName = "jvm/src/test/resources/courses-S18.tsv"
+  val scheduleMap = ScheduleSource.fromFile(fName)
+
+  "A ScheduleSourceSpec"  should "construct a map of schedules from a local file" in  {
+    val expectedSemesters = 2
+    assert(scheduleMap.size == expectedSemesters)
+  }
+
+  it should "key the map by valid and unique semester codes" in {
+    val expectedKeys = Set("F17", "S18")
+    assert(scheduleMap.keySet == expectedKeys)
+
+    for (k <- scheduleMap.keySet) {
+      assert(Semester.forCode(k).size == 1)
+    }
+  }
 
 
 }
