09a81f9cd78bea7e4da0243d8b2c9c79c49d541f
Neel Smith <dnsmith.neel@gmail.com>
2018-01-15 16:13:09 -0500

    Rm shared projet setup

--- shared/src/main/scala/Instructor.scala
+++
@@ -1,8 +0,0 @@
-package edu.holycross.shot.courses
-
-
-/** An instructor of a course.
-*
-* @param name Name of instructor.
-*/
-case class Instructor(name: String)
--- shared/src/test/scala/CourseDaysSpec.scala
+++
@@ -1,9 +0,0 @@
-package edu.holycross.shot.courses
-import org.scalatest.FlatSpec
-
-class CourseDaysSpec extends FlatSpec {
-
-  "A CourseDays value"  should "do something" in pending
-
-  
-}
